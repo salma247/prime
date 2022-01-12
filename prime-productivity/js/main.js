a1d96075f0c93d70727c7a8fadffaef0b4029d25
Salma Ahmed <55872989+salma247@users.noreply.github.com>
2022-01-12 20:42:55 +0200

    Delete prime-productivity directory

--- prime-productivity/js/main.js
+++
@@ -1,6 +0,0 @@
-const toggleBtn = document.getElementsByClassName('toggle-button')[0];
-const navLinks =  document.getElementsByClassName('navbar-links')[0];
-
-toggleBtn.addEventListener('click', ()=>{
-    navLinks.classList.toggle('active')
-})
